ca2e912424e257a02aa8923817ca3feb9e2267a0
Rachna Sha <rsha@novus.com>
2022-05-10 12:58:50 -0500

    Added error handling for invalid number of arguments. #time 20m

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file = &args[1];
+    if args.len() < 2 {
+        println!("Filename not provided. Please rerun passing json filename as an argument.");
+        process::exit(1);
+    }
 
-    if let Err(e) = json_yaml::from_file(&file) {
+    if let Err(e) = json_yaml::from_file(&args[1]) {
         println!("Error parsing json {}", e);
     }
 }
